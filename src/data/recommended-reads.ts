9dd31822b12ec3f837070d43810fd9c86640ed8d
whyself <1158820305@qq.com>
2026-04-06 10:47:35 +0800

    Add Xuanwu Lake essay post with images and recent pick entry

--- src/data/recommended-reads.ts
+++ src/data/recommended-reads.ts
@@ -6,6 +6,7 @@ export interface RecommendedRead {
 }
 
 export const recommendedReads: RecommendedRead[] = [
+  {title: "玄武湖旁的柳树",href: "/posts/玄武湖旁的柳树"},
   {title: "个人博客搭建记",href: "/posts/个人博客搭建记"},
   {title: "Gifted",href: "/posts/gifted"},
 ];
